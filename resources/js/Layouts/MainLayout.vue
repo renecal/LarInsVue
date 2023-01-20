aa226f90b50550b564d21be4f9da2b735e853c56
renecal <rene.calfin@gmail.com>
2023-01-20 14:03:29 -0300

    Your First SPA / 007-eslint-jsconfig

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,17 +1,17 @@
 <template>
-    <Link href="/">Main Page</Link>&nbsp;
-    <Link href="/hello">Show Page</Link>
-    <div>The page with time {{ timer }}</div>
-    <slot></slot>
+  <Link href="/">Main Page</Link>&nbsp;
+  <Link href="/hello">Show Page</Link>
+  <div>The page with time {{ timer }}</div>
+  <slot />
 </template>
 
 <script setup>
-    import { Link } from "@inertiajs/vue3";
-    import { ref } from "vue";
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
-    const timer = ref(0);   
+const timer = ref(0)   
 
-    setInterval(() => {
-        timer.value++
-    }, 1000);
+setInterval(() => {
+  timer.value++
+}, 1000)
 </script>
