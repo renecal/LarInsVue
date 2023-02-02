703d47ae37ae02b5417358c2ed6ffd392801d4b6
renecal <rene.calfin@gmail.com>
2023-02-02 18:57:50 -0300

    004-013-sharing-data-middleware pt2

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,17 +3,17 @@
   <Link href="/hello">Show Page</Link>&nbsp;
   <Link href="/listing">Listing Page</Link>&nbsp;
   <Link href="/listing/create">New Element</Link>&nbsp;
-  <div>The page with time {{ timer }}</div>
+  <!-- <div>The page with time {{ timer }}</div> -->
   <slot />
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
-const timer = ref(0)   
+// const timer = ref(0)   
 
-setInterval(() => {
-  timer.value++
-}, 1000)
+// setInterval(() => {
+//   timer.value++
+// }, 1000)
 </script>
